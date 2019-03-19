4376fd8a17edf3b1414a45d06a49b54275d236f7
Simon Klein <simon.klein.sz.1@gmail.com>
2019-03-19 17:10:58 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 
 #https://support.rackspace.com/how-to/installing-mysql-server-on-ubuntu/
+##Das root passwort muss "root" sein.
 sudo apt-get update
 sudo apt-get install mysql-server
 sudo ufw allow mysql
@@ -10,3 +11,8 @@ systemctl start mysql
 #https://stackoverflow.com/questions/32754461/how-to-install-mysql-connector-via-pip
 sudo -H pip install --upgrade pip
 pip install mysql-connector
+
+
+
+# Datenbank erstellen
+cat file.sql | mysql --user="root" --password="root"
